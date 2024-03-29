d5e3e3a7d63ed247d555b2eb6e5c963708856782
Trojan hax <124779020+Trojanhax@users.noreply.github.com>
2024-03-29 15:26:43 +0530

    Update flask_simple.py

--- 3-Python-C2-Server/2-Flask-basics/flask_simple.py
+++ 3-Python-C2-Server/2-Flask-basics/flask_simple.py
@@ -1,1 +1,21 @@
 
+import flask
+from flask import *
+import json
+
+app = Flask(__name__)   
+@app.route("/" , methods=[ "GET" , "POST"])  
+def index():
+    
+    return render_template('index.html')
+
+
+@app.route("/results" , methods=[ "POST","GET"])
+def results():
+    name = request.form['username_value']
+    return render_template( 'index.html' , name=name )
+    
+    
+
+if __name__  == '__main__':
+    app.run( port=1345 ,debug=True)  
